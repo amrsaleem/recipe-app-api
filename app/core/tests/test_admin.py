d4aaa5f34574f376fd562f3f15173bb954a250b2
Amr Saleem <amr.2010100@gmail.com>
2022-09-03 11:15:41 +0300

    Fixed errors & typos

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -42,6 +42,6 @@ class AdminSiteTests(TestCase):
     def test_create_user_page(self):
         """Teste the create user page works"""
         url = reverse('admin:core_user_add')
-        res = self.client(url)
+        res = self.client.get(url)
 
-        self.assertEqual(res.sataus_code, 200)
+        self.assertEqual(res.status_code, 200)
